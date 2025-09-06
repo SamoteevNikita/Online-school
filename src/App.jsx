59a821f1fd8c6b5b2cf2818e50c05a2840a873a1
Nikita <samoteev.nv@gmail.com>
2025-09-06 23:16:04 +0300

    my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import CoursesPage from "./pages/CoursesPage";
@@ -6,7 +6,7 @@ import TeachersPage from "./pages/TeachersPage";
 import ContactsPage from "./pages/ContactsPage";
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
@@ -15,7 +15,7 @@ function App() {
           <Route path="contacts" element={<ContactsPage />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
